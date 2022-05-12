cd483529104b69e8218d350e574b9bde38158c98
Vũ Nhật Nam <86916560+invisible1501@users.noreply.github.com>
2022-05-13 02:00:24 +0700

    fix errors on server

--- server/server.js
+++ server/server.js
@@ -1,14 +1,10 @@
 const express = require('express');
-const path = require("path");
 const app = express();
+const path = require("path");
 const jsonServer = require('json-server');
-const user = require('./api/users/GET.json');
+const goods = require('./api/goods/GET.json');
 
 /* Json server */
-
-const server = jsonServer.create();
-const router = jsonServer.router(user);
-const middlewares = jsonServer.defaults();
 
 //const bodyParser = require('body-parser');
 //const apiMocker = require('connect-api-mocker');
@@ -16,21 +12,27 @@ var cors = require('cors');
 
 const PORT = process.env.PORT || 8080;
 
-server.use(jsonServer.bodyParser)
+app.use(jsonServer.bodyParser);
+app.use(express.static(path.join(__dirname, '../client/build')));
 //app.use('/api', apiMocker('api'));
-app.use(express.static(path.resolve(__dirname, "../client/build")));
+/* if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+    app.get('*', function (req, res) {
+      res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+    });
+} */
 
-server.use(middlewares);
-server.use('/api', router);
-server.use(cors({
+//app.use(middlewares);
+app.use(cors({
     origin: '*'
 }));
+app.use('/api', jsonServer.router(goods));
 
 //const signRouter = require('./routes/routes');
 
 //app.use('/api/users', signRouter);
 
-server.listen(PORT, (err) => {
+app.listen(PORT, (err) => {
     if(err) throw err;
     console.log(`Server is now running on port ${PORT}`);
 });
