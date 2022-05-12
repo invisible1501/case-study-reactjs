ffc4c9a5fc2c1dcef3c3be749955a8a7c2513574
Vũ Nhật Nam <86916560+invisible1501@users.noreply.github.com>
2022-05-13 02:02:52 +0700

    Update server.js

--- server/server.js
+++ server/server.js
@@ -4,25 +4,13 @@ const path = require("path");
 const jsonServer = require('json-server');
 const goods = require('./api/goods/GET.json');
 
-/* Json server */
-
 //const bodyParser = require('body-parser');
 //const apiMocker = require('connect-api-mocker');
 var cors = require('cors');
-
 const PORT = process.env.PORT || 8080;
 
 app.use(jsonServer.bodyParser);
-app.use(express.static(path.join(__dirname, '../client/build')));
-//app.use('/api', apiMocker('api'));
-/* if(process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build')));
-    app.get('*', function (req, res) {
-      res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
-    });
-} */
-
-//app.use(middlewares);
+app.use(express.static(path.join(__dirname, '../client/build')));  // THIS LINE IS USED FOR DEPLOYING AND I'M NOT REALLY UNDERSTAND WHY
 app.use(cors({
     origin: '*'
 }));
